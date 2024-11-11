301e52f4ce01416317e2f3d92ba13a062a66b0b2
anabrollan <anabrollan@gmail.com>
2024-11-11 12:11:37 -0300

    Agrego los botones y funciones para agregar al carrito

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const ItemCount = ({ stock, addItem }) => {  
+    const [count, setCount] = useState(1);
+
+    const handleClickSubtract = () => {
+        if(count > 1) {
+            setCount(count - 1);
+        }
+    };
+
+    const handleClickAdd = () => {
+        if(count < stock) {
+            setCount(count + 1);
+        }
+    };
+
+    return (
+        <div>
+            <button onClick={handleClickSubtract}>-</button>
+            <button onClick={handleClickAdd}>+</button>
+            <p>{count}</p>
+            <button onClick={() => addItem(count)}>Agregar al carrito</button>{}
+        </div>
+    );
+};
+
+export default ItemCount;
--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -1,9 +1,15 @@
 import { useParams } from "react-router-dom";
-import { products } from "../../data/data.js" 
+import { products } from "../../data/data.js"; 
+import ItemCount from "../ItemCount/ItemCount.jsx";
 
 function ItemDetail() {
   const { idProduct } = useParams();
   const product = products.find((item) => item.id === idProduct);
+
+  const addItem = (count) => {
+    const productCart = { ...product, quantity: count }
+    console.log(`Se agregaron ${count} producto(s) al carrito`);  
+  };
 
   if (!product) {
     return <h2>Producto no encontrado</h2>;
@@ -16,6 +22,7 @@ function ItemDetail() {
       <p className="card">{product.description}</p>
       <p className="card">Precio: ${product.price}</p>
       <p className="card">Stock disponible: {product.stock}</p>
+      <ItemCount stock={product.stock} addItem={addItem} />  {}
     </div>
   );
 }
