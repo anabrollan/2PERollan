d0993559870fe6361e520a225614a4fc0b25457c
anabrollan <anabrollan@gmail.com>
2024-11-11 16:03:21 -0300

    Agrego funcionalidad para borrar productos

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import { useContext } from 'react'
 import { CartContext } from '../../context/CartContext'
 
 const Cart = () => {
-  const { cart } = useContext(CartContext)
+  const { cart, totalPrice, deleteProductById } = useContext(CartContext)
   return (
     <div>
       <h2>Productos en tu carrito</h2>
@@ -13,11 +13,14 @@ const Cart = () => {
             <img className="image" src={productCart.image} />
             <p>{productCart.name}</p>
             <p>Cantidad: {productCart.quantity}</p>
-            <p>Precio unitario: {productCart.price}</p>
-            <p>Subtotal: {productCart.quantity * productCart.price}</p>
+            <p>Precio unitario: {productCart.price}$</p>
+            <p>Subtotal: {productCart.quantity * productCart.price}$</p>
+            <button onClick={ () => deleteProductById(productCart.id) }>Eliminar producto</button>
           </div>
         ))
       }
+
+      <p>Total: {totalPrice()}$</p>
     </div>
   )
 }
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -14,8 +14,18 @@ const CartProvider = ( { children } ) => {
         return quantity
     }
 
+    const totalPrice = () => {
+        const price = cart.reduce( (total, productCart) => total + ( productCart.quantity * productCart.price), 0 )
+        return price
+    }
+
+    const deleteProductById = (idProduct) => {
+        const filterProducts = cart.filter( (productCart) => productCart.id !== idProduct )
+        setCart(filterProducts)
+    }
+
     return(
-        <CartContext.Provider value={ { cart, addProductInCart, totalQuantity } } >
+        <CartContext.Provider value={ { cart, addProductInCart, totalQuantity, totalPrice, deleteProductById } } >
             {children}
         </CartContext.Provider>
     )
