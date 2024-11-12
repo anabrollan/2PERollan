7ae0b9b191e46b7bd06c383788b2094db7c959cd
anabrollan <anabrollan@gmail.com>
2024-11-12 14:25:56 -0300

    Agrego el filtrado por categorías

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,38 +1,51 @@
-import { useState, useEffect } from "react"
-import { getProducts } from "../../data/data.js"
-import '../ItemListContainer/ItemListContainer.css'
-import { useParams } from "react-router-dom"
-import ItemList from "./ItemList.jsx"
+import { useState, useEffect } from "react";
+import '../ItemListContainer/ItemListContainer.css';
+import { useParams } from "react-router-dom";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import db from "../../db/db.js";
+import ItemList from "./ItemList.jsx";
 
-const ItemListContainer = ( {saludo} ) => {
-    const [products, setProducts] = useState([])
-    const { idCategory } = useParams()
+const ItemListContainer = ({ saludo }) => {
+    const [products, setProducts] = useState([]);
+    const { idCategory } = useParams();
 
-    useEffect( ()=> {
-        getProducts ()
-        .then((data) => {
-            if(idCategory){
-                const filterProducts = data.filter( (product)=> product.category === idCategory )
-                setProducts(filterProducts)
-            }else{
-                setProducts(data)
-            }
-            
-            
-        })
-        .catch((error) => {
-            console.error(error)
-        })
-        .finally(() => {
-            console.log("fin")
-        })}, [idCategory])
+    const getProducts = () => {
+        const productsRef = collection(db, "products");
+        getDocs(productsRef)
+            .then((dataDb) => {
+                const productsArray = dataDb.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+                setProducts(productsArray);
+            })
+            .catch((error) => console.error("No se ha encontrado el producto", error));
+    };
+
+    const getProductsByCategory = () => {
+        const productsRef = collection(db, "products");
+        const queryCategories = query(productsRef, where("category", "==", idCategory));
+        getDocs(queryCategories)
+            .then((dataDb) => {
+                const productsDb = dataDb.docs.map((doc) => {
+                    return { id: doc.id, ...doc.data() };
+                });
+                setProducts(productsDb);
+            })
+            .catch((error) => console.error("No se ha encontrado el producto", error));
+    };
+
+    useEffect(() => {
+        if (idCategory) {
+            getProductsByCategory();
+        } else {
+            getProducts();
+        }
+    }, [idCategory]);
 
     return (
         <div className="item-list-container">
             <h2>{saludo}</h2>
             <ItemList products={products} />
         </div>
-    )
-}
+    );
+};
 
-export default ItemListContainer
+export default ItemListContainer;
