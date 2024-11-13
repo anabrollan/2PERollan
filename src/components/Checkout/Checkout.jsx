e2942aa2adb65139ed511c55119c506c29059897
anabrollan <anabrollan@gmail.com>
2024-11-13 19:45:45 -0300

    Agrego la base del formulario

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Checkout = () => {
+  const [dataForm, setDataForm] = useState ({
+    fullname: "",
+    phone: "",
+    email: ""
+  })
+
+  const handleChangeInput = (event) => {
+    setDataForm( { ...dataForm, [event.target.name]: event.target.value } )
+  }
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault()
+  }
+
   return (
-    <div>Checkout</div>
+    <div>
+      <form onSubmit={handleSubmitForm}>
+        <label>Nombre completo: </label>
+        <input type="text" name="fullname" value={dataForm.fullname} onChange={handleChangeInput} />
+
+        <label>Teléfono: </label>
+        <input type="number" name="phone" value={dataForm.phone} onChange={handleChangeInput} />
+
+        <label>Correo: </label>
+        <input type="email" name="email" value={dataForm.email} onChange={handleChangeInput} />
+
+        <button type="submit">Terminar pedido</button>
+      </form>
+    </div>
   )
 }
 
