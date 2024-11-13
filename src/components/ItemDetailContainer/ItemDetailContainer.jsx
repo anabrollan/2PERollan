290451316dcbc116a4158c20f9c9a649c9c3277a
anabrollan <anabrollan@gmail.com>
2024-11-13 09:34:04 -0300

    Vuelvo a intentar arreglar itemdetailcontainer

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,29 +1,14 @@
 import { useState, useEffect } from "react";
-import { doc, getDoc } from "firebase/firestore";
-import db from "../../db/db.js";
 import { getProducts } from "../../data/data";
 import { useParams } from "react-router-dom";
-import ItemDetail from "./ItemDetail.jsx";
 
 const ItemDetailContainer = () => {
     const [product, setProduct] = useState ({})
 
-const { idProduct } = useParams()
-
-    const getProductById = () => {
-        const docRef = doc( db, "products", idProduct )
-        getDoc(docRef)
-            .then((dataDb)=> {
-                
-                const productDb = { id: dataDb.id, ...dataDb.data() }
-
-                setProduct(productDb)
-            })
-    }
 
     useEffect( ()=> {
-        getProductById()
-    },[idProduct])
+        
+    }, [idProduct] )
 
     return (
         <ItemDetail product={product} />
